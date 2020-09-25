2b31444fe91b8a4303eb9085e915c5c884afafa9
Alex Hendrie Bielen <alexhendriebielen@gmail.com>
2020-09-25 17:07:50 -0400

    Add README and update diagrams.
    Why: Add a README to explain the architecture. Move around the diagram
    images and add some image management code.

--- docs/architecture/diagrams/architecture-aws.py
+++ docs/architecture/diagrams/architecture-aws.py
@@ -1,16 +1,20 @@
-from diagrams import Diagram, Cluster, Edge
+import os
+import pathlib
+
+from diagrams import Cluster, Diagram, Edge
 from diagrams.aws.analytics import Athena
 from diagrams.aws.compute import Fargate, Lambda
-from diagrams.aws.database import Dynamodb
-from diagrams.aws.integration import Eventbridge, SQS
+from diagrams.aws.database import DocumentDB
+from diagrams.aws.integration import SQS, Eventbridge
 from diagrams.aws.network import APIGateway
 from diagrams.aws.storage import S3
-from diagrams.generic.network import Firewall
 from diagrams.generic.device import Mobile
+from diagrams.generic.network import Firewall
 from diagrams.programming.language import NodeJS
 
+filename = "architecture-aws"
 
-with Diagram("Site Scanner", show=False):
+with Diagram("Site Scanner", show=False, filename="architecture-aws"):
 
     with Cluster("API"):
         api_data_gov = Firewall("api.data.gov") # not really a firewall
@@ -22,10 +26,10 @@ with Diagram("Site Scanner", show=False):
 
 
     with Cluster("Data and Storage"):
-        dynamo = Dynamodb("DynamoDB")
+        documentdb = DocumentDB("DocumentDB")
 
         with Cluster("Cold Storage Logic"):
-            dynamo_lambda = Lambda("Lambda")
+            document_lambda = Lambda("Lambda")
             cold_storage_node = NodeJS("Cold Storage Code")
 
         s3_cold_storage = S3("S3 (Cold Storage)")
@@ -41,19 +45,30 @@ with Diagram("Site Scanner", show=False):
 
         queue = SQS("Consumer Queue")
 
-        with Cluster("Serverless Fargate Cluster"):
-            fargate = [Fargate("Fargate 1...n")]
+        with Cluster("Lambda Consumers"):
+            consumer_lambda = [Lambda("Lambda 1...n")]
             node_apps = NodeJS("Scanning Logic")
-
-    
-
 
 
     cron >> Edge(label="triggers") >> producer >> Edge(label="publishes to") >> queue 
-    queue << Edge(label="subscribes to") << fargate >> Edge(label="writes to") >> dynamo 
-    dynamo >> Edge(label="ttl triggers") >> dynamo_lambda >> Edge(label="writes to") >> s3_cold_storage
+    queue << Edge(label="subscribes to") << consumer_lambda >> Edge(label="writes to") >> documentdb
+    documentdb >> Edge(label="ttl triggers") >> document_lambda>> Edge(label="writes to") >> s3_cold_storage
     athena >> Edge(label="queries") >> s3_cold_storage
-    api_data_gov >> Edge(label="manages") >> api_gateway >> Edge(label="calls") >> api_lambda >> Edge(label="reads from") >> dynamo
-    Mobile(label="The Public") >> Edge(label="calls") >> api_data_gov
+    api_data_gov >> Edge(label="manages") >> api_gateway >> Edge(label="calls") >> api_lambda >> Edge(label="reads from") >> documentdb
+
+
+
+filename_with_extension = filename + ".png"
+while not os.path.exists(filename_with_extension):
+    pass
+
+file_path = pathlib.Path(__file__).parent.absolute()
+os.rename(file_path.joinpath(filename_with_extension), file_path.joinpath("images").joinpath(filename_with_extension))
+
+
+
+
+
+
     
     
