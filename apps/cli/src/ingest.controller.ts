e9d709153b40a182ffb3a67026327b75f9a77a1f
Alex Hendrie Bielen <alexhendriebielen@gmail.com>
2020-10-29 17:21:39 -0400

    Add a dev option to the ingest cli app.

--- apps/cli/src/ingest.controller.ts
+++ apps/cli/src/ingest.controller.ts
@@ -5,7 +5,13 @@ import { Controller } from '@nestjs/common';
 export class IngestController {
   constructor(private readonly ingestService: IngestService) {}
 
-  async writeUrls() {
-    this.ingestService.writeUrls();
+  async writeUrls(dev?: boolean) {
+    const urls = await this.ingestService.getUrls();
+
+    if (dev) {
+      await this.ingestService.writeUrls(urls, 20);
+    } else {
+      await this.ingestService.writeUrls(urls);
+    }
   }
 }
--- apps/cli/src/main.ts
+++ apps/cli/src/main.ts
@@ -8,11 +8,16 @@ async function bootstrap() {
   return app;
 }
 
-async function ingest() {
+async function ingest(cmdObj) {
   const nestApp = await bootstrap();
   const controller = nestApp.get(IngestController);
   console.log('ingesting target urls');
-  await controller.writeUrls();
+
+  if (cmdObj.dev) {
+    await controller.writeUrls(true);
+  } else {
+    await controller.writeUrls();
+  }
 }
 
 async function main() {
@@ -26,6 +31,7 @@ async function main() {
   program
     .command('ingest')
     .description('ingest adds target urls to the Website database table')
+    .option('--dev', 'limits the ingest service to the first 20 urls', false)
     .action(ingest);
 
   await program.parseAsync(process.argv);
