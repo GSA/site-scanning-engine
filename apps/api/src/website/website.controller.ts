06a8fb78021e991ead0bdf0a5e37b80a38130673
Alex Hendrie Bielen <alexhendriebielen@gmail.com>
2020-11-24 17:40:23 -0500

    Cleanup the API

--- apps/api/src/website/website.controller.ts
+++ apps/api/src/website/website.controller.ts
@@ -1,6 +1,5 @@
-import { CreateWebsiteDto } from '@app/database/websites/dto/create-website.dto';
 import { WebsiteService } from '@app/database/websites/websites.service';
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { websiteSerializer } from './serializer';
 import { map } from 'lodash';
 
@@ -14,10 +13,5 @@ export class WebsiteController {
     const serialized = map(websites, websiteSerializer);
 
     return serialized;
-  }
-
-  @Post()
-  async createWebsite(@Body() createWebsiteDto: CreateWebsiteDto) {
-    await this.websiteService.create(createWebsiteDto);
   }
 }
