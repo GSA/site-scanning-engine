16c43bffeae7fc83d59ba87f675ffe742d916f70
Alex Hendrie Bielen <alexhendriebielen@gmail.com>
2020-12-03 15:23:42 -0500

    Add validation pipe.

--- libs/database/src/websites/dto/filter-website.dto.ts
+++ libs/database/src/websites/dto/filter-website.dto.ts
@@ -1,6 +1,15 @@
+import { IsBooleanString, IsUrl } from 'class-validator';
+
 export class FilterWebsiteDto {
+  @IsUrl()
   target_url_domain?: string;
+
+  @IsUrl()
   final_url_domain?: string;
+
+  @IsBooleanString()
   final_url_live?: boolean;
+
+  @IsBooleanString()
   target_url_redirects?: boolean;
 }
