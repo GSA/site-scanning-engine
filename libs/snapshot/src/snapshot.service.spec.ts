f630022fce556694168cee9ce92b7837cab5bf7c
Alex Hendrie Bielen <alexhendriebielen@gmail.com>
2020-12-15 18:10:01 -0500

    Fix test.

--- libs/snapshot/src/snapshot.service.spec.ts
+++ libs/snapshot/src/snapshot.service.spec.ts
@@ -1,15 +1,24 @@
+import { DatabaseModule } from '@app/database';
+import { LoggerModule } from '@app/logger';
+import { StorageModule } from '@app/storage';
 import { Test, TestingModule } from '@nestjs/testing';
 import { SnapshotService } from './snapshot.service';
 
 describe('SnapshotService', () => {
   let service: SnapshotService;
+  let module: TestingModule;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    module = await Test.createTestingModule({
+      imports: [StorageModule, LoggerModule, DatabaseModule],
       providers: [SnapshotService],
     }).compile();
 
     service = module.get<SnapshotService>(SnapshotService);
+  });
+
+  afterAll(async () => {
+    await module.close();
   });
 
   it('should be defined', () => {
