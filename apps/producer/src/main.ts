a95ab72c9884166a442fb44862fb8609e365e11b
Alex Hendrie Bielen <alexhendriebielen@gmail.com>
2021-01-05 19:28:32 -0500

    Add logging and change memory and instance number.

--- apps/producer/src/main.ts
+++ apps/producer/src/main.ts
@@ -3,7 +3,12 @@ import { ProducerModule } from './producer.module';
 import { TaskService } from './task/task.service';
 
 async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(ProducerModule);
+  const app = await NestFactory.createApplicationContext(ProducerModule, {
+    logger:
+      process.env.NODE_ENV === 'dev'
+        ? ['log', 'error', 'warn', 'debug', 'verbose']
+        : ['error', 'warn'],
+  });
   const taskService = app.get(TaskService);
 
   await taskService.start();
--- apps/scan-engine/src/main.ts
+++ apps/scan-engine/src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(AppModule);
+  const app = await NestFactory.createApplicationContext(AppModule, {
+    logger:
+      process.env.NODE_ENV === 'dev'
+        ? ['log', 'error', 'warn', 'debug', 'verbose']
+        : ['error', 'warn'],
+  });
   app.init();
 }
 bootstrap();
