b3108fbf636d9b6603e514e4a6af6557118f0eca
Alex Hendrie Bielen <alexhendriebielen@gmail.com>
2021-12-06 15:18:51 -0500

    Update to configuration to get API KEY.

--- apps/api/src/website/config/api.config.ts
+++ apps/api/src/website/config/api.config.ts
@@ -4,7 +4,12 @@
 export default () => {
   if (process.env.VCAP_SERVICES) {
     const vcap = JSON.parse(process.env.VCAP_SERVICES);
-    const apiKey = vcap['API_KEY'][0];
+    const apiKey = vcap.hasOwnProperty('user-provided')
+      ? vcap['user-provided'][0]['credentials']['API_KEY']
+      : undefined;
+    if (apiKey === undefined) {
+      throw 'No API_KEY Found';
+    }
     return {
       apiKey: apiKey,
     };
