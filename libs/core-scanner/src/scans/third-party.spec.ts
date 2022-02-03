48755e286e9cecb0d34cce00e168456f9a22a9cc
Daniel Naab <dan@crushingpennies.com>
2022-02-03 14:06:49 -0600

    Add third-party unit test; add back solutions scanner e2e tests - to be revisited with recent changes.

--- libs/core-scanner/src/scans/third-party.spec.ts
+++ libs/core-scanner/src/scans/third-party.spec.ts
@@ -1,3 +1,53 @@
+import { mock } from 'jest-mock-extended';
+import { Logger } from 'pino';
+import { HTTPRequest, HTTPResponse, Page } from 'puppeteer';
+
+import { newTestPage } from '../test-helper';
+import { buildThirdPartyResult } from './third-party';
+
 describe('third-party scan', () => {
-  xit('works', () => {});
+  let page: Page;
+  let response: HTTPResponse;
+  beforeAll(async () => {
+    ({ page, response } = await newTestPage());
+  });
+
+  it('non-navigation different domains treated as third-parties', async () => {
+    expect(
+      await buildThirdPartyResult(
+        mock<HTTPResponse>({
+          url: () => 'https://www.18f.gov/',
+        }),
+        [
+          mock<HTTPRequest>({
+            url: () => 'https://www.18f.gov/',
+            isNavigationRequest: () => false,
+          }),
+          mock<HTTPRequest>({
+            url: () => 'https://www.18f.gov/',
+            isNavigationRequest: () => true,
+          }),
+          mock<HTTPRequest>({
+            url: () => 'https://google.com/',
+            isNavigationRequest: () => false,
+          }),
+          mock<HTTPRequest>({
+            url: () => 'https://test.com/',
+            isNavigationRequest: () => false,
+          }),
+          mock<HTTPRequest>({
+            url: () => 'https://navrequest1.com/',
+            isNavigationRequest: () => true,
+          }),
+          mock<HTTPRequest>({
+            url: () => 'https://navrequest2.com/',
+            isNavigationRequest: () => true,
+          }),
+        ],
+      ),
+    ).toEqual({
+      thirdPartyServiceCount: 2,
+      thirdPartyServiceDomains: 'google.com,test.com',
+    });
+  });
 });
--- libs/core-scanner/test/app.e2e-spec.ts
+++ libs/core-scanner/test/app.e2e-spec.ts
@@ -1,7 +1,11 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
 import { CoreScannerModule, CoreScannerService } from '@app/core-scanner';
 import { CoreInputDto } from '@app/core-scanner/core.input.dto';
-import { Test, TestingModule } from '@nestjs/testing';
+import { ScanStatus } from '@app/core-scanner/scan-status';
+
 import { CoreResult } from 'entities/core-result.entity';
+import { SolutionsResult } from 'entities/solutions-result.entity';
 import { Website } from 'entities/website.entity';
 
 describe('CoreScanner (e2e)', () => {
@@ -74,5 +78,85 @@ describe('CoreScanner (e2e)', () => {
     const result = await service.scan(input);
     expect(result.coreResult).toStrictEqual(expected);
     //expect(result.solutionsResult).toStrictEqual({});
+  });
+});
+
+describe('SolutionsScanner (e2e)', () => {
+  let service: CoreScannerService;
+  let moduleFixture: TestingModule;
+
+  beforeEach(async () => {
+    moduleFixture = await Test.createTestingModule({
+      imports: [CoreScannerModule],
+    }).compile();
+
+    service = moduleFixture.get<CoreScannerService>(CoreScannerService);
+  });
+
+  afterAll(async () => {
+    //await service.onModuleDestroy();
+    await moduleFixture.close();
+  });
+
+  it('returns results for a url', async () => {
+    const input: CoreInputDto = {
+      websiteId: 1,
+      url: '18f.gov',
+      scanId: '123',
+    };
+    const website = new Website();
+    website.id = input.websiteId;
+
+    const expected = new SolutionsResult();
+    expected.website = website;
+    expected.usaClasses = 50;
+    expected.uswdsString = 1;
+    expected.uswdsTables = 0;
+    expected.uswdsInlineCss = 0;
+    expected.uswdsUsFlag = 20;
+    expected.uswdsStringInCss = 20;
+    expected.uswdsUsFlagInCss = 0;
+    expected.uswdsMerriweatherFont = 5;
+    expected.uswdsPublicSansFont = 20;
+    expected.uswdsSourceSansFont = 5;
+    expected.uswdsSemanticVersion = '2.9.0';
+    expected.uswdsVersion = 20;
+    expected.uswdsCount = 141;
+    expected.status = ScanStatus.Completed;
+    expected.dapDetected = true;
+    expected.dapParameters = 'agency=GSA&subagency=TTS%2C18F';
+    expected.ogTitleFinalUrl = '18F: Digital service delivery | Home';
+    expected.ogDescriptionFinalUrl =
+      '18F builds effective, user-centric digital services focused on the interaction between government and the people and businesses it serves.';
+    expected.ogArticlePublishedFinalUrl = undefined;
+    expected.ogArticleModifiedFinalUrl = undefined;
+    expected.mainElementFinalUrl = false;
+    expected.robotsTxtDetected = true;
+    expected.robotsTxtStatusCode = 200;
+    expected.robotsTxtFinalUrl = 'https://18f.gsa.gov/robots.txt';
+    expected.robotsTxtFinalUrlLive = true;
+    expected.robotsTxtFinalUrlMimeType = 'text/plain';
+    expected.robotsTxtTargetUrlRedirects = true;
+    expected.robotsTxtFinalUrlSize = 65;
+    expected.robotsTxtCrawlDelay = undefined;
+    expected.robotsTxtSitemapLocations = 'https://18f.gsa.gov/sitemap.xml';
+    expected.sitemapXmlDetected = true;
+    expected.sitemapXmlStatusCode = 200;
+    expected.sitemapXmlFinalUrl = 'https://18f.gsa.gov/sitemap.xml';
+    expected.sitemapXmlFinalUrlLive = true;
+    expected.sitemapTargetUrlRedirects = true;
+    expected.sitemapXmlFinalUrlMimeType = 'application/xml';
+    expected.sitemapXmlPdfCount = 0;
+    expected.thirdPartyServiceCount = 3;
+    expected.thirdPartyServiceDomains =
+      'dap.digitalgov.gov,fonts.googleapis.com,www.google-analytics.com';
+
+    const result = await service.scan(input);
+
+    // these values change frequently so just add them to the expected object.
+    expected.sitemapXmlFinalUrlFilesize = result.sitemapXmlFinalUrlFilesize;
+    expected.sitemapXmlCount = result.sitemapXmlCount;
+
+    expect(result).toStrictEqual(expected);
   });
 });
