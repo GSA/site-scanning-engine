d3488b5c617abf8d2d58f970ceeb920e39144cb4
Andy Kuny <andrew.kuny@gsa.gov>
2022-09-14 09:00:56 -0400

    Add IPv6 failure test case to DNS scan

--- libs/core-scanner/src/pages/dns.spec.ts
+++ libs/core-scanner/src/pages/dns.spec.ts
@@ -7,4 +7,8 @@ describe('dns scan', () => {
   it('integrates with node.js dns module', async () => {
     expect(await dns.dnsScan(mock<Logger>(), 'gsa.gov')).toEqual(true);
   });
+
+  it('catches an error when the scanned site has no IPv6 record available', async () => {
+    expect(await dns.dnsScan(mock<Logger>(), 'github.com')).toEqual(false);
+  });
 });
