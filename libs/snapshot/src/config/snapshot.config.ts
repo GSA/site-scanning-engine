dc3b96fd98df3ca093f46d89fe9ed94de82ad5c3
Andy Kuny <andrew.kuny@gsa.gov>
2022-12-16 08:47:33 -0500

    Invert comparison in snapshot.config.ts

--- libs/snapshot/src/config/snapshot.config.ts
+++ libs/snapshot/src/config/snapshot.config.ts
@@ -2,15 +2,15 @@
  * returns the appropriate snapshot filenames for the environment.
  */
 export default () => {
-  if (process.env.NODE_ENV !== 'production') {
-    return {
-      fileNameLive: 'weekly-snapshot-staging',
-      fileNameAll: 'weekly-snapshot-all-staging',
-    };
-  } else {
+  if (process.env.NODE_ENV === 'production') {
     return {
       fileNameLive: 'weekly-snapshot',
       fileNameAll: 'weekly-snapshot-all',
+    };
+  } else {
+    return {
+      fileNameLive: 'weekly-snapshot-staging',
+      fileNameAll: 'weekly-snapshot-all-staging',
     };
   }
 };
