2eaf87d8eece08a3a8bf1049057459ad322e598d
Andy Kuny <andrew.kuny@gsa.gov>
2023-02-08 14:31:13 -0500

    Refactor cloud.gov pages scan

--- libs/core-scanner/src/scans/cloud-dot-gov-pages.ts
+++ libs/core-scanner/src/scans/cloud-dot-gov-pages.ts
@@ -5,15 +5,11 @@ import { CloudDotGovPagesScan } from 'entities/scan-data.entity';
 export const buildCloudDotGovPagesResult = async (
   mainResponse: HTTPResponse,
 ): Promise<CloudDotGovPagesScan> => {
-  let cloudDotGovPages = false;
   const headers = mainResponse.headers();
-
-  if (
-    headers['x-server'] &&
-    headers['x-server'].toLowerCase() === 'cloud.gov pages'
-  ) {
-    cloudDotGovPages = true;
-  }
+  const cloudDotGovPages =
+    (headers['x-server'] &&
+      headers['x-server'].toLowerCase() === 'cloud.gov pages') ??
+    false;
 
   return {
     cloudDotGovPages,
