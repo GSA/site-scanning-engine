1d26d1c301c1fd55eb8eedfea6814136a75d4565
Andy Kuny <andrew.kuny@gsa.gov>
2023-02-15 10:07:39 -0500

    Update dns scan

--- libs/core-scanner/src/pages/dns.ts
+++ libs/core-scanner/src/pages/dns.ts
@@ -24,18 +24,40 @@ const ipv6Scan = async (hostname, logger) => {
 };
 
 const hostnameScan = async (hostname, logger) => {
+  const cnameLookupResult = await cnameLookup(hostname, logger);
+  const reverseLookupResult = await reverseLookup(hostname, logger);
+
+  if (usesCloudService(cnameLookupResult)) return cnameLookupResult;
+  if (usesCloudService(reverseLookupResult)) return reverseLookupResult;
+
+  logger.info({ msg: 'No results from hostnameScan' });
+  return null;
+};
+
+const reverseLookup = async (hostname, logger) => {
   try {
     const addresses = await dns.resolve(hostname);
     const hostnames = await dns.reverse(addresses[0]);
     const domain = hostnames[0].split('.').slice(-2).join('.');
-    return usesCloudService(domain) ? domain : null;
+    return domain;
   } catch (error) {
-    logger.info({ msg: 'Error during cloud scan:', error });
+    logger.info({ msg: 'Error during reverse lookup:', error });
+    return null;
+  }
+};
+
+const cnameLookup = async (hostname, logger) => {
+  try {
+    const cnameResolution = await dns.resolveCname(hostname);
+    return cnameResolution[0].split('.').slice(-2).join('.');
+  } catch (error) {
+    logger.info({ msg: 'Error during cname lookup:', error });
     return null;
   }
 };
 
 const usesCloudService = (domain): boolean => {
+  if (!domain) return false;
   if (domain === 'cloud.gov' || domain === 'data.gov') return true;
 
   return cloudServiceStrings.some((string) => domain.includes(string));
