b0f7a2a505790157262df218ca5f5224a853efa0
Andy Kuny <andrew.kuny@gsa.gov>
2023-03-07 09:34:23 -0500

    Update HSTS interceptor

--- apps/api/src/hsts.interceptor.ts
+++ apps/api/src/hsts.interceptor.ts
@@ -13,7 +13,10 @@ export class HstsInterceptor implements NestInterceptor {
     return next.handle().pipe(
       tap(() => {
         const res = context.switchToHttp().getResponse();
-        res.header('Strict-Transport-Security', 'max-age=31536000; preload');
+        res.header(
+          'Strict-Transport-Security',
+          'max-age=31536000; includeSubDomains; preload',
+        );
       }),
     );
   }
