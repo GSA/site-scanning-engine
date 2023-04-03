7d5dbd1f16903e5ed84434863e70991463614847
Andy Kuny <andrew.kuny@gsa.gov>
2023-04-03 11:13:25 -0400

    Add basic robots-txt and sitemap-xml tests

---
+++ libs/core-scanner/src/pages/sitemap-xml.spec.ts
@@ -0,0 +1,61 @@
+import { mock, MockProxy } from 'jest-mock-extended';
+import { Logger } from 'pino';
+import { Page, HTTPRequest, HTTPResponse } from 'puppeteer';
+
+import { CoreInputDto } from '../core.input.dto';
+import { createSitemapXmlScanner } from './sitemap-xml';
+import { source } from './test-page-source';
+
+describe('sitemap-xml scanner', () => {
+  let mockPage: MockProxy<Page>;
+  let mockRequest: MockProxy<HTTPRequest>;
+  let redirectRequest: MockProxy<HTTPRequest>;
+  let mockResponse: MockProxy<HTTPResponse>;
+  let mockLogger: MockProxy<Logger>;
+  const finalUrl = 'https://18f.gsa.gov';
+
+  beforeEach(async () => {
+    mockPage = mock<Page>();
+    mockResponse = mock<HTTPResponse>();
+    mockRequest = mock<HTTPRequest>();
+    redirectRequest = mock<HTTPRequest>();
+    mockLogger = mock<Logger>();
+
+    redirectRequest.url.calledWith().mockReturnValue('https://18f.gov');
+    mockRequest.redirectChain.calledWith().mockReturnValue([redirectRequest]);
+    mockResponse.request.calledWith().mockReturnValue(mockRequest);
+    mockResponse.status.calledWith().mockReturnValue(200);
+    mockResponse.headers.calledWith().mockReturnValue({
+      'Content-Type': 'text/html; charset=utf-8',
+    });
+    mockPage.goto.calledWith('https://18f.gov').mockResolvedValue(mockResponse);
+    mockPage.url.calledWith().mockReturnValue(finalUrl);
+  });
+
+  it('should scan for a sitemap-xml page', async () => {
+    const input: CoreInputDto = {
+      websiteId: 1,
+      url: '18f.gov',
+      scanId: '123',
+    };
+
+    mockResponse.text.mockResolvedValue(source);
+    mockResponse.url.mockReturnValue('https://18f.gsa.gov');
+    mockPage.goto.mockResolvedValue(mockResponse);
+    redirectRequest.redirectChain.mockReturnValue([]);
+
+    const scanner = createSitemapXmlScanner(mockLogger, input);
+    const result = await scanner(mockPage);
+
+    expect(result).toEqual({
+      sitemapXmlScan: {
+        sitemapXmlFinalUrl: 'https://18f.gsa.gov/',
+        sitemapXmlFinalUrlLive: true,
+        sitemapTargetUrlRedirects: true,
+        sitemapXmlFinalUrlMimeType: 'text/html',
+        sitemapXmlStatusCode: 200,
+        sitemapXmlDetected: false,
+      },
+    });
+  });
+});
