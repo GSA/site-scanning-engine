f3357975bc79c9c148e6cb691d6fcfd3d2f60fe0
Andy Kuny <andrew.kuny@gsa.gov>
2023-04-24 11:19:32 -0400

    Update and refactor required links scan

--- libs/core-scanner/src/scans/required-links.ts
+++ libs/core-scanner/src/scans/required-links.ts
@@ -6,43 +6,40 @@ export const buildRequiredLinksResult = async (
   mainResponse: HTTPResponse,
 ): Promise<RequiredLinksScan> => {
   const html = await mainResponse.text();
-  const requiredLinksUrl = [];
-  const requiredLinksText = [];
 
-  if (hasStringInHref(html, 'about')) requiredLinksUrl.push('about');
-  if (hasStringInHref(html, 'fear')) requiredLinksUrl.push('fear');
-  if (hasStringInHref(html, 'foia')) requiredLinksUrl.push('foia');
-  if (hasStringInHref(html, 'inspector')) requiredLinksUrl.push('inspector');
-  if (hasStringInHref(html, 'privacy')) requiredLinksUrl.push('privacy');
-  if (hasStringInHref(html, 'usa.gov')) requiredLinksUrl.push('usa.gov');
+  const requiredLinksUrl = [
+    'about',
+    'fear',
+    'foia',
+    'inspector',
+    'privacy',
+    'usa.gov',
+  ]
+    .filter((string) => hasStringInHref(html, string))
+    .join(',');
 
-  if (hasStringInLinkText(html, 'about us')) requiredLinksText.push('about');
-  if (hasStringInLinkText(html, 'accessibility'))
-    requiredLinksText.push('accessibility');
-  if (hasStringInLinkText(html, 'budget and performance'))
-    requiredLinksText.push('budget');
-  if (hasStringInLinkText(html, 'no fear act')) requiredLinksText.push('fear');
-  if (
-    hasStringInLinkText(html, 'foia') ||
-    hasStringInLinkText(html, 'freedom of information act')
-  )
-    requiredLinksText.push('foia');
-  if (hasStringInLinkText(html, 'inspector general'))
-    requiredLinksText.push('inspector');
-  if (hasStringInLinkText(html, 'privacy policy'))
-    requiredLinksText.push('privacy');
-  if (hasStringInLinkText(html, 'vulnerability disclosure'))
-    requiredLinksText.push('vulnerability');
+  const requiredLinksText = [
+    'about us',
+    'accessibility',
+    'budget and performance',
+    'no fear act',
+    'foia',
+    'freedom of information act',
+    'inspector general',
+    'privacy policy',
+    'vulnerability disclosure',
+  ]
+    .filter((string) => hasStringInLinkText(html, string))
+    .join(',');
 
   return {
-    requiredLinksUrl: requiredLinksUrl.join(','),
-    requiredLinksText: requiredLinksText.join(','),
+    requiredLinksUrl,
+    requiredLinksText,
   };
 };
 
 const hasStringInHref = (html: string, string: string): boolean => {
-  const hrefRegex = new RegExp(/href=['"](.*?)['"]/g);
-  const hrefs = html.match(hrefRegex);
+  const hrefs = html.match(/href=['"](.*?)['"]/gi);
   if (!hrefs) return false;
   const matchingHrefs = hrefs.filter((href) =>
     href.toLowerCase().includes(string),
@@ -51,8 +48,7 @@ const hasStringInHref = (html: string, string: string): boolean => {
 };
 
 const hasStringInLinkText = (html: string, string: string): boolean => {
-  const linkTextRegex = new RegExp(/<a[^>]*>(.*?)<\/a>/g);
-  const linkTexts = html.match(linkTextRegex);
+  const linkTexts = html.match(/(?<=<a\b[^>]*>)(.*?)(?=<\/a>)/gi);
   if (!linkTexts) return false;
   const matchingLinkTexts = linkTexts.filter((linkText) =>
     linkText.toLowerCase().includes(string),
