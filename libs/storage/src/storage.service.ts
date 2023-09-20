1ccfbdbbbf835a28b632e02b74bfc366481e4c2b
Andy Kuny <andrew.kuny@gsa.gov>
2023-09-20 15:48:48 -0400

    Update aws-sdk dependency

--- libs/storage/src/storage.service.ts
+++ libs/storage/src/storage.service.ts
@@ -1,20 +1,21 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import * as AWS from 'aws-sdk';
+import { S3 } from '@aws-sdk/client-s3';
 
 @Injectable()
 export class StorageService {
-  private s3: AWS.S3;
+  private s3: S3;
   private bucket: string;
   private logger = new Logger(StorageService.name);
 
   constructor(private configService: ConfigService) {
-    this.s3 = new AWS.S3({
+    this.s3 = new S3({
       endpoint: this.configService.get<string>('s3.endpoint'),
-      accessKeyId: this.configService.get<string>('s3.accessKeyId'),
-      secretAccessKey: this.configService.get<string>('s3.secretAccessKey'),
-      s3ForcePathStyle: true,
-      signatureVersion: 'v4',
+      credentials: {
+        accessKeyId: this.configService.get<string>('s3.accessKeyId'),
+        secretAccessKey: this.configService.get<string>('s3.secretAccessKey'),
+      },
+      forcePathStyle: true,
     });
 
     this.bucket = this.configService.get<string>('s3.bucketName');
@@ -23,13 +24,11 @@ export class StorageService {
   async upload(fileName: string, body: string) {
     this.logger.debug('attempting s3 putObject request...');
     try {
-      await this.s3
-        .putObject({
-          Bucket: this.bucket,
-          Key: fileName,
-          Body: body,
-        })
-        .promise();
+      await this.s3.putObject({
+        Bucket: this.bucket,
+        Key: fileName,
+        Body: body,
+      });
       this.logger.debug('putObject request completed');
     } catch (error) {
       const err = error as Error;
@@ -40,13 +39,11 @@ export class StorageService {
   async copy(from: string, to: string) {
     this.logger.debug('attempting s3 copyObject request...');
     try {
-      await this.s3
-        .copyObject({
-          Bucket: this.bucket,
-          CopySource: `${this.bucket}/${from}`,
-          Key: to,
-        })
-        .promise();
+      await this.s3.copyObject({
+        Bucket: this.bucket,
+        CopySource: `${this.bucket}/${from}`,
+        Key: to,
+      });
       this.logger.debug('copyObject request completed');
     } catch (error) {
       const err = error as Error;
