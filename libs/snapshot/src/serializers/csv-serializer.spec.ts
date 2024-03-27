76eeeee42c1df506421baaac2a1f4b4e02d3b2e9
Andy Kuny <andrew.kuny@gsa.gov>
2024-03-27 08:43:23 -0400

    Move snapshot column order to static property of CoreResult entity

--- libs/snapshot/src/serializers/csv-serializer.spec.ts
+++ libs/snapshot/src/serializers/csv-serializer.spec.ts
@@ -3,95 +3,8 @@ import { Website } from 'entities/website.entity';
 import { CsvSerializer } from './csv-serializer';
 
 describe('CsvSerializer', () => {
-  let liveColumnOrder: string[];
-
-  beforeEach(() => {
-    liveColumnOrder = [
-      'target_url',
-      'target_url_domain',
-      'target_url_top_level_domain',
-      'target_url_redirects',
-      'final_url',
-      'final_url_domain',
-      'final_url_top_level_domain',
-      'final_url_website',
-      'final_url_live',
-      'final_url_status_code',
-      'final_url_media_type',
-      'final_url_same_domain',
-      'final_url_same_website',
-      'target_url_agency_owner',
-      'target_url_bureau_owner',
-      'target_url_branch',
-      'target_url_404_test',
-      'source_list',
-      'public',
-      'scan_date',
-      'primary_scan_status',
-      'accessibility_scan_status',
-      'dns_scan_status',
-      'not_found_scan_status',
-      'performance_scan_status',
-      'robots_txt_scan_status',
-      'sitemap_xml_scan_status',
-      'ipv6',
-      'hostname',
-      'cms',
-      'login_provider',
-      'login',
-      'site_search',
-      'search_dot_gov',
-      'dap',
-      'dap_parameters',
-      'third_party_service_domains',
-      'third_party_service_count',
-      'cookie_domains',
-      'viewport_meta_tag',
-      'cumulative_layout_shift',
-      'largest_contentful_paint',
-      'required_links_url',
-      'required_links_text',
-      'title',
-      'description',
-      'og_title',
-      'og_description',
-      'og_article_published',
-      'og_article_modified',
-      'canonical_link',
-      'main_element_present',
-      'robots_txt_detected',
-      'robots_txt_target_url_redirects',
-      'robots_txt_final_url',
-      'robots_txt_final_url_live',
-      'robots_txt_final_url_status_code',
-      'robots_txt_final_url_media_type',
-      'robots_txt_final_url_filesize',
-      'robots_txt_crawl_delay',
-      'robots_txt_sitemap_locations',
-      'sitemap_xml_detected',
-      'sitemap_xml_target_url_redirects',
-      'sitemap_xml_final_url',
-      'sitemap_xml_final_url_live',
-      'sitemap_xml_final_url_status_code',
-      'sitemap_xml_final_url_media_type',
-      'sitemap_xml_final_url_filesize',
-      'sitemap_xml_count',
-      'sitemap_xml_pdf_count',
-      'uswds_favicon',
-      'uswds_favicon_in_css',
-      'uswds_publicsans_font',
-      'uswds_inpage_css',
-      'uswds_usa_classes',
-      'uswds_string',
-      'uswds_string_in_css',
-      'uswds_semantic_version',
-      'uswds_version',
-      'uswds_count',
-    ];
-  });
-
   it('returns headers when no data is passed', () => {
-    const serializer = new CsvSerializer(liveColumnOrder);
+    const serializer = new CsvSerializer(CoreResult.snapshotColumnOrder);
 
     const result = serializer.serialize([]);
     const expectedResult =
@@ -101,7 +14,7 @@ describe('CsvSerializer', () => {
   });
 
   it('serializes an array containing one website', () => {
-    const serializer = new CsvSerializer(liveColumnOrder);
+    const serializer = new CsvSerializer(CoreResult.snapshotColumnOrder);
     const website = new Website();
     const coreResult = new CoreResult();
     website.url = '18f.gov';
@@ -115,7 +28,7 @@ describe('CsvSerializer', () => {
   });
 
   it('serializes an array containing more than one website', () => {
-    const serializer = new CsvSerializer(liveColumnOrder);
+    const serializer = new CsvSerializer(CoreResult.snapshotColumnOrder);
     const firstWebsite = new Website();
     const firstCoreResult = new CoreResult();
     firstWebsite.url = '18f.gov';
@@ -133,7 +46,7 @@ describe('CsvSerializer', () => {
   });
 
   it('serializes an array containing one website with dap parameters', () => {
-    const serializer = new CsvSerializer(liveColumnOrder);
+    const serializer = new CsvSerializer(CoreResult.snapshotColumnOrder);
     const website = new Website();
     const coreResult = new CoreResult();
     coreResult.dapParameters = 'agency=GSA&subagency=TTS,18F';
@@ -148,7 +61,7 @@ describe('CsvSerializer', () => {
   });
 
   it('serializes an array containing more than one website with dap parameters', () => {
-    const serializer = new CsvSerializer(liveColumnOrder);
+    const serializer = new CsvSerializer(CoreResult.snapshotColumnOrder);
     const firstWebsite = new Website();
     const firstCoreResult = new CoreResult();
     firstCoreResult.dapParameters = 'agency=GSA&subagency=TTS,18F';
@@ -169,7 +82,7 @@ describe('CsvSerializer', () => {
   });
 
   it('serializes a csv file and removes newline characters in the process', () => {
-    const serializer = new CsvSerializer(liveColumnOrder);
+    const serializer = new CsvSerializer(CoreResult.snapshotColumnOrder);
     const website = new Website();
     const coreResult = new CoreResult();
     coreResult.ogDescriptionFinalUrl = 'Yadda \nYadda \nYadda';
@@ -184,7 +97,7 @@ describe('CsvSerializer', () => {
   });
 
   it('serializes a csv file and truncates strings that contain more than 5000 characters', () => {
-    const serializer = new CsvSerializer(liveColumnOrder);
+    const serializer = new CsvSerializer(CoreResult.snapshotColumnOrder);
     const website = new Website();
     const coreResult = new CoreResult();
     const longString = generateLongString(6000);
