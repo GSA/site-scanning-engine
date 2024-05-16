6852c768f16c7584bf1964dbb10a751a822edb70
Andy Kuny <andrew.kuny@gsa.gov>
2024-05-16 10:20:14 -0400

    Expand range of errors detected in scan-status

--- entities/scan-status.ts
+++ entities/scan-status.ts
@@ -6,15 +6,31 @@ export enum ScanStatus {
   InvalidSSLCert = 'invalid_ssl_cert',
   ConnectionRefused = 'connection_refused',
   ConnectionReset = 'connection_reset',
+  ConnectionClosed = 'connection_closed',
+  AddressUnreachable = 'address_unreachable',
+  SslVersionCipherMismatch = 'ssl_version_cipher_mismatch',
+  EmptyResponse = 'empty_response',
+  Http2Error = 'http2_error',
+  TooManyRedirects = 'too_many_redirects',
+  ExecutionContextDestroyed = 'execution_context_destroyed',
+  PageFrameNotReady = 'page_frame_not_ready',
+  EvaluationFailed = 'evaluation_failed',
 }
 
 export const parseBrowserError = (err: Error): ScanStatus => {
-  if (err.name === 'TimeoutError') {
+  if (
+    err.name === 'TimeoutError' ||
+    err.message.startsWith('net::ERR_CONNECTION_TIMED_OUT') ||
+    err.message.startsWith('connect ETIMEDOUT')
+  ) {
     return ScanStatus.Timeout;
   }
 
   if (err.message) {
-    if (err.message.startsWith('net::ERR_NAME_NOT_RESOLVED')) {
+    if (
+      err.message.startsWith('net::ERR_NAME_NOT_RESOLVED') ||
+      err.message.startsWith('getaddrinfo ENOTFOUND')
+    ) {
       return ScanStatus.DNSResolutionError;
     }
 
@@ -27,12 +43,54 @@ export const parseBrowserError = (err: Error): ScanStatus => {
       return ScanStatus.InvalidSSLCert;
     }
 
-    if (err.message.startsWith('net::ERR_CONNECTION_REFUSED')) {
+    if (
+      err.message.startsWith('net::ERR_CONNECTION_REFUSED') ||
+      err.message.startsWith('connect ECONNREFUSED')
+    ) {
       return ScanStatus.ConnectionRefused;
     }
 
-    if (err.message.startsWith('net::ERR_CONNECTION_RESET')) {
+    if (
+      err.message.startsWith('net::ERR_CONNECTION_RESET') ||
+      err.message.startsWith('read ECONNRESET')
+    ) {
       return ScanStatus.ConnectionReset;
+    }
+
+    if (err.message.startsWith('net::ERR_CONNECTION_CLOSED')) {
+      return ScanStatus.ConnectionClosed;
+    }
+
+    if (err.message.startsWith('net::ERR_ADDRESS_UNREACHABLE')) {
+      return ScanStatus.AddressUnreachable;
+    }
+
+    if (err.message.startsWith('net::ERR_SSL_VERSION_OR_CIPHER_MISMATCH')) {
+      return ScanStatus.SslVersionCipherMismatch;
+    }
+
+    if (err.message.startsWith('net::ERR_EMPTY_RESPONSE')) {
+      return ScanStatus.EmptyResponse;
+    }
+
+    if (err.message.startsWith('net::ERR_HTTP2_PROTOCOL_ERROR')) {
+      return ScanStatus.Http2Error;
+    }
+
+    if (err.message.startsWith('net::ERR_TOO_MANY_REDIRECTS')) {
+      return ScanStatus.TooManyRedirects;
+    }
+
+    if (err.message.startsWith('Execution context was destroyed')) {
+      return ScanStatus.ExecutionContextDestroyed;
+    }
+
+    if (err.message.startsWith('Page/Frame is not ready')) {
+      return ScanStatus.PageFrameNotReady;
+    }
+
+    if (err.message.startsWith('Evaluation failed')) {
+      return ScanStatus.EvaluationFailed;
     }
   }
 
