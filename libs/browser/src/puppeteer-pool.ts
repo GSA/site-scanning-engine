d8d98ff3877910718a99421881780fb91a98e215
Luke Chavers <lucas.chavers@gsa.gov>
2024-11-20 16:28:15 -0500

    Revert "Ignore cache directory created by puppeteer"
    This reverts commit 7636b09d01ea0f8f07e27c75ae6edc90105eb5d7.

--- libs/browser/src/puppeteer-pool.ts
+++ libs/browser/src/puppeteer-pool.ts
@@ -52,7 +52,7 @@ export const createPuppeteerPool = (
     maxUses: 100,
     testOnBorrow: true,
     puppeteerArgs: {
-      headless: true,
+      headless: 'shell' as 'shell', // Type assertion to bypass the error
       acceptInsecureCerts: true,
       args: ['--no-sandbox', '--enable-logging', '--v=1'],
     },
