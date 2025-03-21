1ceacc7e52feba1145f919cb7d98b2a01b74b654
Luke Chavers <lucas.chavers@gsa.gov>
2025-03-20 21:29:22 -0400

    Additional metadata added to status scans

--- entities/scan-status.ts
+++ entities/scan-status.ts
@@ -37,7 +37,7 @@ export const parseBrowserError = (err: Error, logger: Logger): AnyFailureStatus 
         err.message.startsWith('connect ETIMEDOUT') ||
         err.message.startsWith('net::ERR_TIMED_OUT')))
   ) {
-    childLogger.warn({timeoutError : true}, `Timeout: ${err.message}`);
+    childLogger.warn({timeoutError : true, errorReturn: ScanStatus.Timeout}, `Timeout: ${err.message}`);
     return ScanStatus.Timeout;
   }
 
@@ -46,7 +46,7 @@ export const parseBrowserError = (err: Error, logger: Logger): AnyFailureStatus 
       err.message.startsWith('net::ERR_NAME_NOT_RESOLVED') ||
       err.message.startsWith('getaddrinfo ENOTFOUND')
     ) {
-      childLogger.warn({dnsResolutionError: true}, `DNS resolution error: ${err.message}`);
+      childLogger.warn({dnsResolutionError: true, errorReturn: ScanStatus.DNSResolutionError}, `DNS resolution error: ${err.message}`);
       return ScanStatus.DNSResolutionError;
     }
 
@@ -57,7 +57,7 @@ export const parseBrowserError = (err: Error, logger: Logger): AnyFailureStatus 
       err.message.startsWith('net::ERR_SSL_UNRECOGNIZED_NAME_ALERT') ||
       err.message.startsWith('unable to verify the first certificate')
     ) {
-      childLogger.warn({invalidSSLCertError: true}, `Invalid SSL cert: ${err.message}`);
+      childLogger.warn({invalidSSLCertError: true, errorReturn: ScanStatus.InvalidSSLCert}, `Invalid SSL cert: ${err.message}`);
       return ScanStatus.InvalidSSLCert;
     }
 
@@ -65,7 +65,7 @@ export const parseBrowserError = (err: Error, logger: Logger): AnyFailureStatus 
       err.message.startsWith('net::ERR_CONNECTION_REFUSED') ||
       err.message.startsWith('connect ECONNREFUSED')
     ) {
-      childLogger.warn({connectionRefusedError: true}, `Connection refused: ${err.message}`);
+      childLogger.warn({connectionRefusedError: true, errorReturn: ScanStatus.ConnectionRefused}, `Connection refused: ${err.message}`);
       return ScanStatus.ConnectionRefused;
     }
 
@@ -73,81 +73,81 @@ export const parseBrowserError = (err: Error, logger: Logger): AnyFailureStatus 
       err.message.startsWith('net::ERR_CONNECTION_RESET') ||
       err.message.startsWith('read ECONNRESET')
     ) {
-      childLogger.warn({connectionResetError: true}, `Connection reset: ${err.message}`);
+      childLogger.warn({connectionResetError: true, errorReturn: ScanStatus.ConnectionReset}, `Connection reset: ${err.message}`);
       return ScanStatus.ConnectionReset;
     }
 
     if (err.message.startsWith('net::ERR_CONNECTION_CLOSED')) {
-      childLogger.warn({connectionClosedError: true}, `Connection closed: ${err.message}`);
+      childLogger.warn({connectionClosedError: true, errorReturn: ScanStatus.ConnectionClosed}, `Connection closed: ${err.message}`);
       return ScanStatus.ConnectionClosed;
     }
 
     if (err.message.startsWith('net::ERR_ADDRESS_UNREACHABLE')) {
-      childLogger.warn({addressUnreachableError: true}, `Address unreachable: ${err.message}`);
+      childLogger.warn({addressUnreachableError: true, errorReturn: ScanStatus.AddressUnreachable}, `Address unreachable: ${err.message}`);
       return ScanStatus.AddressUnreachable;
     }
 
     if (err.message.startsWith('net::ERR_SSL_VERSION_OR_CIPHER_MISMATCH')) {
-      childLogger.warn({sslVersionCipherMismatchError: true}, `SSL version/cipher mismatch: ${err.message}`);
+      childLogger.warn({sslVersionCipherMismatchError: true, errorReturn: ScanStatus.SslVersionCipherMismatch}, `SSL version/cipher mismatch: ${err.message}`);
       return ScanStatus.SslVersionCipherMismatch;
     }
 
     if (err.message.startsWith('net::ERR_EMPTY_RESPONSE')) {
-      childLogger.warn({emptyResponseError: true}, `Empty response: ${err.message}`);
+      childLogger.warn({emptyResponseError: true, errorReturn: ScanStatus.EmptyResponse}, `Empty response: ${err.message}`);
       return ScanStatus.EmptyResponse;
     }
 
     if (err.message.startsWith('net::ERR_HTTP2_PROTOCOL_ERROR')) {
-      childLogger.warn({http2ErrorError: true}, `HTTP2 error: ${err.message}`);
+      childLogger.warn({http2ErrorError: true, errorReturn: ScanStatus.Http2Error}, `HTTP2 error: ${err.message}`);
       return ScanStatus.Http2Error;
     }
 
     if (err.message.startsWith('net::ERR_TOO_MANY_REDIRECTS')) {
-      childLogger.warn({tooManyRedirectsError: true}, `Too many redirects: ${err.message}`);
+      childLogger.warn({tooManyRedirectsError: true, errorReturn: ScanStatus.TooManyRedirects}, `Too many redirects: ${err.message}`);
       return ScanStatus.TooManyRedirects;
     }
 
     if (err.message.startsWith('Execution context was destroyed')) {
-      childLogger.warn({executionContextDestroyedError: true}, `Execution context was destroyed: ${err.message}`);
+      childLogger.warn({executionContextDestroyedError: true, errorReturn: ScanStatus.ExecutionContextDestroyed}, `Execution context was destroyed: ${err.message}`);
       return ScanStatus.ExecutionContextDestroyed;
     }
 
     if (err.message.startsWith('Page/Frame is not ready')) {
-      childLogger.warn({pageFrameNotReady: true}, `Page/Frame is not ready: ${err.message}`);
+      childLogger.warn({pageFrameNotReady: true, errorReturn: ScanStatus.PageFrameNotReady}, `Page/Frame is not ready: ${err.message}`);
       return ScanStatus.PageFrameNotReady;
     }
 
     if (err.message.startsWith('Evaluation failed')) {
-      childLogger.warn({evaluationFailedError: true}, `Evaluation failed: ${err.message}`);
+      childLogger.warn({evaluationFailedError: true, errorReturn: ScanStatus.EvaluationFailed}, `Evaluation failed: ${err.message}`);
       return ScanStatus.EvaluationFailed;
     }
 
     if (err.message.startsWith('net::ERR_INVALID_RESPONSE')) {
-      childLogger.warn({invalidResponseError: true}, `Invalid response: ${err.message}`);
+      childLogger.warn({invalidResponseError: true, errorReturn: ScanStatus.InvalidResponse}, `Invalid response: ${err.message}`);
       return ScanStatus.InvalidResponse;
     }
 
     if (err.message.startsWith('net::ERR_INVALID_AUTH_CREDENTIALS')) {
-      childLogger.warn({invalidAuthCredentialsError: true}, `Invalid auth credentials: ${err.message}`);
+      childLogger.warn({invalidAuthCredentialsError: true, errorReturn: ScanStatus.InvalidAuthCredentials}, `Invalid auth credentials: ${err.message}`);
       return ScanStatus.InvalidAuthCredentials;
     }
 
     if (err.message.startsWith('net::ERR_SSL_PROTOCOL_ERROR')) {
-      childLogger.warn({sslProtocolErrorError: true}, `SSL protocol error: ${err.message}`);
+      childLogger.warn({sslProtocolErrorError: true, errorReturn: ScanStatus.SslProtocolError}, `SSL protocol error: ${err.message}`);
       return ScanStatus.SslProtocolError;
     }
 
     if (err.message.startsWith('net::ERR_ABORTED')) {
-      childLogger.warn({abortedError: true}, `Aborted: ${err.message}`);
+      childLogger.warn({abortedError: true, errorReturn: ScanStatus.Aborted}, `Aborted: ${err.message}`);
       return ScanStatus.Aborted;
     }
 
     if (err.toString() === 'Processing timed out') {
-      childLogger.warn({timeoutError: true}, `Timeout: ${err.message}`);
+      childLogger.warn({timeoutError: true, errorReturn: ScanStatus.Timeout}, `Timeout: ${err.message}`);
       return ScanStatus.Timeout;
     }
   }
-  childLogger.warn({unknownError: true}, `Unknown error: ${err.message}`);
+  childLogger.warn({unknownError: true, errorReturn: ScanStatus.UnknownError}, `Unknown error: ${err.message}`);
 
   return ScanStatus.UnknownError;
 };
