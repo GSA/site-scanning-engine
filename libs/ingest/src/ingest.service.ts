56a37a16e5d3fc24701e995cdad5c0b710eee3f9
YaritzaGarcia <yaritza.garcia@gsa.gov>
2025-09-08 10:16:01 -0400

    Add 'sourceListPublicInventory' to headers

--- libs/ingest/src/ingest.service.ts
+++ libs/ingest/src/ingest.service.ts
@@ -60,6 +60,7 @@ export class IngestService {
         'sourceListUsagovClicksMil',
         'sourceListSearchGov',
         'sourceListSearchGovMil',
+        'sourceListPublicInventory',
         'filtered',
         'pageviews',
         'visits',
