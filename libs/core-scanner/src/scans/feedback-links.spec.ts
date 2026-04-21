e6ca1ecd966894b26fbf8c7294da631eb59d4428
Andrew Kuny <akuny@flexion.us>
2026-04-21 10:00:02 -0400

    1828: update unit test

--- libs/core-scanner/src/scans/feedback-links.spec.ts
+++ libs/core-scanner/src/scans/feedback-links.spec.ts
@@ -2,10 +2,12 @@ import { browserInstance, newTestPage } from '../test-helper';
 import { buildFeedbackLinksResult } from './feedback-links';
 import pino from 'pino';
 
+const mockLogger = pino();
+
 describe('feedback links scan', () => {
   it('detects feedback links strings in href attribute and a element text', async () => {
     await newTestPage(async ({ page }) => {
-      expect(await buildFeedbackLinksResult(page)).toEqual({
+      expect(await buildFeedbackLinksResult(mockLogger, page)).toEqual({
         feedbackLinksText: '',
       });
     });
