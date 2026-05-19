52d2ffe57539a6fcc3cfaf80e71266c9eca18085
Ethan Gardner <ethan.gardner@gsa.gov>
2026-05-19 14:22:39 -0400

    only mock the minimum methods

--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -26,6 +26,7 @@ jest.mock('dns', () => {
   return {
     ...actualDns,
     promises: {
+      ...actualDns.promises,
       resolve6: jest.fn((hostname: string) => {
         if (hostname === 'gsa.gov') {
           return Promise.resolve(['2001:0db8:85a3:0000:0000:8a2e:0370:7334']);
@@ -37,18 +38,6 @@ jest.mock('dns', () => {
           return Promise.resolve(['d2u8q06xshnec9.cloudfront.net.amazonaws.com']);
         }
         return actualDns.promises.resolveCname(hostname);
-      }),
-      resolve: jest.fn((hostname: string) => {
-        if (hostname === 'gsa.gov') {
-          return Promise.resolve(['13.249.70.113']);
-        }
-        return actualDns.promises.resolve(hostname);
-      }),
-      reverse: jest.fn((ip: string) => {
-        if (ip === '13.249.70.113') {
-          return Promise.resolve(['server-13-249-70-113.iad89.r.cloudfront.net']);
-        }
-        return actualDns.promises.reverse(ip);
       }),
     },
   };
